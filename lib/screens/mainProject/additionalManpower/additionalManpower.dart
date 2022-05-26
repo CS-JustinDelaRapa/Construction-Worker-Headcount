16b2c5ecc9c67a8e18684c566774ba3903f2be7d
Randy Lingat Jr <randy.lingat@hoolisoftware.com>
2022-05-26 22:29:54 +0800

    additional manpower

--- lib/screens/mainProject/additionalManpower/additionalManpower.dart
+++ lib/screens/mainProject/additionalManpower/additionalManpower.dart
@@ -18,21 +18,174 @@ class _AdditionalManpowerState extends State<AdditionalManpower> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.transparent,
-      appBar: AppBar(
-        leading: OpenDrawerWidget(
-          onClicked: widget.openDrawer,
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          leading: OpenDrawerWidget(
+            onClicked: widget.openDrawer,
+          ),
+          title: const Text('Additional Manpower'),
         ),
-        title: const Text('Additional Manpower'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            CustomWidgets().text_title('Additional Manpower', 35),
-          ],
-        ),
-      ),
-    );
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: DataTable(
+              headingRowHeight: MediaQuery.of(context).size.height * 0.12,
+              dataRowHeight: MediaQuery.of(context).size.height * 0.2,
+              showBottomBorder: true,
+              dividerThickness: 3.0,
+              border: TableBorder.all(
+                width: 1.0,
+                color: Colors.black,
+              ),
+              columns: <DataColumn>[
+                DataColumn(
+                    label: SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.1,
+                  child: const Text(
+                    'Item No.',
+                    textAlign: TextAlign.center,
+                    overflow: TextOverflow.visible,
+                    softWrap: true,
+                  ),
+                )),
+                DataColumn(
+                  label: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.31,
+                    child: const Text(
+                      'Labor-related Factor',
+                      textAlign: TextAlign.center,
+                      overflow: TextOverflow.visible,
+                      softWrap: true,
+                    ),
+                  ),
+                ),
+                DataColumn(
+                  label: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.2,
+                    child: const Text(
+                      'Additional Percentage to the Manpower Distribution',
+                      overflow: TextOverflow.visible,
+                      softWrap: true,
+                    ),
+                  ),
+                ),
+              ],
+              rows: const <DataRow>[
+                DataRow(
+                  cells: <DataCell>[
+                    DataCell(Center(child: Text('1'))),
+                    DataCell(Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Center(
+                        child: Text(
+                            'Shortage of construction workers due to increase in numbers of construction projects'),
+                      ),
+                    )),
+                    DataCell(Padding(
+                        padding: EdgeInsets.all(8.0), child: Text('30%'))),
+                  ],
+                ),
+                DataRow(
+                  cells: <DataCell>[
+                    DataCell(Center(child: Text('2'))),
+                    DataCell(Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Center(
+                        child: Text(
+                            'Unskilled construction workers due to lack of experience and training'),
+                      ),
+                    )),
+                    DataCell(Padding(
+                        padding: EdgeInsets.all(8.0), child: Text('20%'))),
+                  ],
+                ),
+                DataRow(
+                  cells: <DataCell>[
+                    DataCell(Center(child: Text('3'))),
+                    DataCell(Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Center(
+                        child: Text(
+                            'Language barrier between workers of different dialect'),
+                      ),
+                    )),
+                    DataCell(Padding(
+                        padding: EdgeInsets.all(8.0), child: Text('10%'))),
+                  ],
+                ),
+                DataRow(
+                  cells: <DataCell>[
+                    DataCell(Center(child: Text('4'))),
+                    DataCell(Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Center(
+                        child: Text(
+                            'Issues of overtime scheduling with construction workers'),
+                      ),
+                    )),
+                    DataCell(Padding(
+                        padding: EdgeInsets.all(8.0), child: Text('20%'))),
+                  ],
+                ),
+                DataRow(
+                  cells: <DataCell>[
+                    DataCell(Center(child: Text('5'))),
+                    DataCell(Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Center(
+                        child: Text(
+                            'Demand of higher salaries from construction workers'),
+                      ),
+                    )),
+                    DataCell(Padding(
+                        padding: EdgeInsets.all(8.0), child: Text('10%'))),
+                  ],
+                ),
+                DataRow(
+                  cells: <DataCell>[
+                    DataCell(Center(child: Text('6'))),
+                    DataCell(Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Center(
+                        child: Text(
+                            'Struggle with authority leading to conflict between construction workers and engineers'),
+                      ),
+                    )),
+                    DataCell(Padding(
+                        padding: EdgeInsets.all(8.0), child: Text('20%'))),
+                  ],
+                ),
+                DataRow(
+                  cells: <DataCell>[
+                    DataCell(Center(child: Text('7'))),
+                    DataCell(Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Center(
+                        child: Text(
+                            'Sudden absences of construction workers without prior notice to superiors'),
+                      ),
+                    )),
+                    DataCell(Padding(
+                        padding: EdgeInsets.all(8.0), child: Text('30%'))),
+                  ],
+                ),
+                DataRow(
+                  cells: <DataCell>[
+                    DataCell(Center(child: Text('8'))),
+                    DataCell(Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Center(
+                        child: Text(
+                            'Usage of electronic devices during working hours'),
+                      ),
+                    )),
+                    DataCell(Padding(
+                        padding: EdgeInsets.all(8.0), child: Text('20%'))),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
